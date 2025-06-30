a4c5946f23537d5855250c834f625183848987b2
Nayan0406 <nayannikhare83@gmail.com>
2025-06-30 10:41:22 +0530

    Changes add

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -57,15 +57,5 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.get("/:id", async (req, res) => {
-  try {
-    const blog = await Blog.findById(req.params.id);
-    if (!blog) return res.status(404).json({ message: "Blog not found" });
-    res.json({ blog });
-  } catch (err) {
-    res.status(500).json({ message: "Error fetching blog" });
-  }
-});
-
 
 export default router;
