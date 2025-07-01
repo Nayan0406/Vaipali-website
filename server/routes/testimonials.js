d329a2c4811771651b319368ed95e8fcb6043960
Nayan0406 <nayannikhare83@gmail.com>
2025-07-01 12:18:49 +0530

    changes

--- server/routes/testimonials.js
+++ server/routes/testimonials.js
@@ -1,16 +1,22 @@
-import express from 'express';
+import express from 'express'
 import Testimonial from "../models/Testimonial.js";
-
 const router = express.Router();
 
 router.post("/", async (req, res) => {
   const { name, owner, rating, description, image } = req.body;
-  const testimonial = new Testimonial ({ name, owner, rating, description, image });
-  await testimonial.save();
-
   if (!name || !owner || !rating || !description || !image) {
     return res.status(400).json({ message: "All fields are required" });
   }
+try {
+    const testimonial = await Testimonial.create({ name, owner, rating, description, image });
+    console.log("✅ Testimonial received:", req.body);
+    res.status(200).json({ message: "Testimonial added successfully", testimonial });
+  } catch (error) {
+    console.error("❌ Error saving testimonial:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+
+
 });
 
 export default router;
