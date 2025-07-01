bc886098bf934a46f0f320518cc605b5f8e4d72f
Nayan0406 <nayannikhare83@gmail.com>
2025-07-01 11:45:26 +0530

    changes

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import multer from 'multer';
 import Blog from '../models/Blog.js';
-// import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
